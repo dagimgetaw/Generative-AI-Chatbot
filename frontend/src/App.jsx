f7010aa1c5670e2ec175804735f19adf1d0d37fa
Dagim <dagimgetaW27@gmail.com>
2024-12-01 13:30:33 +0300

    add AuthProvider for the logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./components/Main/Main";
+import Signup from "./components/Join/Signup.jsx";
+import Login from "./components/Join/Login.jsx";
+import NotFound from "./components/NotFound/NotFound.jsx";
+import { AuthProvider } from "./AuthContext";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
 };
 
 export default App;
