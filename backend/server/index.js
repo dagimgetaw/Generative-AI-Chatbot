489fbbe5beae3eee3a5dab6b6f7b928aaa7c9e94
Dagim <dagimgetaW27@gmail.com>
2024-12-01 13:38:52 +0300

    add login and signup functionality with MongoDB

---
+++ backend/server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserModel = require("./models/user");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/UserData");
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  UserModel.findOne({ username: username }).then((user) => {
+    if (user) {
+      if (user.password === password) {
+        res.json("success");
+      } else {
+        res.json("password is incorrect");
+      }
+    } else {
+      res.json("no user existed");
+    }
+  });
+});
+
+app.post("/signup", (req, res) => {
+  UserModel.create(req.body)
+    .then((user) => res.json(user))
+    .catch((err) => res.json(err));
+});
+
+app.listen(3001, () => {
+  console.log("Server is running");
+});
