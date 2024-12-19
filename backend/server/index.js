03ad470ac0e1582a469ceff400d06821d4acbf6b
Dagim <dagimgetaW27@gmail.com>
2024-12-19 16:20:44 +0300

    add bcrypt for pass encryption and make username unique

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const bcrypt = require("bcrypt");
 const UserModel = require("./models/user");
 
 const app = express();
@@ -9,27 +10,49 @@ app.use(cors());
 
 mongoose.connect("mongodb://127.0.0.1:27017/UserData");
 
-app.post("/login", (req, res) => {
+// Login Route
+app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  UserModel.findOne({ username: username }).then((user) => {
-    if (user) {
-      if (user.password === password) {
-        res.json("success");
-      } else {
-        res.json("password is incorrect");
-      }
-    } else {
-      res.json("no user existed!");
-    }
-  });
+
+  try {
+    const user = await UserModel.findOne({ username });
+    if (!user) return res.status(404).json("No user exists!");
+
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect)
+      return res.status(401).json("Password is incorrect");
+
+    res.json("success");
+  } catch (err) {
+    res.status(500).json("Server error");
+  }
 });
 
-app.post("/signup", (req, res) => {
-  UserModel.create(req.body)
-    .then((user) => res.json(user))
-    .catch((err) => res.json(err));
+// Signup Route
+app.post("/signup", async (req, res) => {
+  const { username, email, password, repeatPassword } = req.body;
+
+  try {
+    // Check if user already exists
+    const existingUser = await UserModel.findOne({
+      $or: [{ email }, { username }],
+    });
+    if (existingUser) {
+      return res.status(400).json("Username already in use");
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    await UserModel.create({ username, email, password: hashedPassword });
+
+    res.json("success");
+  } catch (err) {
+    res.status(500).json("Error creating user");
+  }
 });
 
 app.listen(3001, () => {
-  console.log("Server is running");
+  console.log("Server is running on port 3001");
 });
