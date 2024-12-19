5ba484fe4127b13792d87f4f3e874af818c6a207
Dagim <dagimgetaW27@gmail.com>
2024-12-19 16:19:59 +0300

    add repeat password table into the DB

--- backend/server/models/user.js
+++ backend/server/models/user.js
@@ -4,6 +4,7 @@ const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  repeatPassword: String,
 });
 
 const UserModel = mongoose.model("UserData", UserSchema);
